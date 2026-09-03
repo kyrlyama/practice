8e0d36cede3b57cbf4e98a52df938ded3c70dbd9
kyrlyama <145273077+kyrlyama@users.noreply.github.com>
2026-09-03 15:47:44 +0300

    Change database connection settings
    Updated database connection details and set charset to utf8mb4.

--- db.php
+++ db.php
@@ -1,7 +1,9 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "control");
+$mysqli = new mysqli("sql211.infinityfree.com", "if0_42814037", "s3E7ye41bYFseqK", "if0_42814037_control_likefon");
 
 if ($mysqli->connect_error) {
   die("Ошибка подключения: " . $mysqli->connect_error);
 }
+
+$mysqli->set_charset("utf8mb4");
 ?>
